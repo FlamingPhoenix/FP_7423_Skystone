9e6e9c798885bb19ae2c0655ce0bb7ad8e33ed1f
Brandon-Tsai <brandon@brandontsai.com>
2019-11-06 21:01:42 -0500

    11/6 Created and programmed a teleop program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTele.java
@@ -0,0 +1,113 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "TeleOp", group = "none")
+public class MyTele extends OpMode {
+    DcMotor fr;
+    DcMotor fl;
+    DcMotor br;
+    DcMotor bl;
+    float x1, x2, y1, y2;
+    DcMotor intakeMotorLeft;
+    DcMotor intakeMotorRight;
+    //Servo puller;
+
+
+    public void drive(float x1, float y1, float x2) {
+        float frontLeft = y1 + x1 + x2;
+        float frontRight = y1 - x1 - x2;
+        float backLeft = y1 - x1 + x2;
+        float backRight = y1 + x1 - x2;
+
+        frontLeft = Range.clip(frontLeft, -1, 1);
+        frontRight = Range.clip(frontRight, -1, 1);
+        backLeft = Range.clip(backLeft, -1, 1);
+        backRight = Range.clip(backRight, -1, 1);
+
+        fl.setPower(frontLeft);
+        fr.setPower(frontRight);
+        bl.setPower(backLeft);
+        br.setPower(backRight);
+    }
+
+    @Override
+    public void init() {
+        fr = hardwareMap.dcMotor.get("frontright");
+        fl = hardwareMap.dcMotor.get("frontleft");
+        br = hardwareMap.dcMotor.get("backright");
+        bl = hardwareMap.dcMotor.get("backleft");
+
+        intakeMotorLeft = hardwareMap.dcMotor.get("intaketh1");
+        intakeMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intakeMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        intakeMotorRight = hardwareMap.dcMotor.get("intaketh2");
+        intakeMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intakeMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        intakeMotorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        /*
+        puller = hardwareMap.servo.get("puller");
+        ServoControllerEx pullerController = (ServoControllerEx) puller.getController();
+        int pullerServoPort = puller.getPortNumber();
+        PwmControl.PwmRange pullerPwmRange = new PwmControl.PwmRange(700, 2300);
+        pullerController.setServoPwmRange(pullerServoPort, pullerPwmRange);
+        puller.setPosition(0.95);
+        */
+
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+
+        x1 = gamepad1.left_stick_x;
+        y1 = gamepad1.left_stick_y;
+        x2 = gamepad1.right_stick_x;
+        y2 = gamepad1.right_stick_y;
+        double joystickLeftDistance = Math.pow(x1, 2) + Math.pow(y1, 2);
+        if (joystickLeftDistance < 0.9)
+        {
+            x1 = x1/2;
+            y1 = y1/2;
+        }
+        double joystickRightDistance = Math.pow(x2, 2) + Math.pow(y2, 2);
+        if (joystickRightDistance < 0.9)
+        {
+            x2 = x2/2;
+        }
+        drive(x1,  y1 * -1, x2);
+
+        if (gamepad1.right_trigger > 0.2) //in
+        {
+            intakeMotorLeft.setPower(0.3);
+            intakeMotorRight.setPower(0.3);
+        }
+        else if (gamepad1.right_bumper) //out
+        {
+            intakeMotorLeft.setPower(-1);
+            intakeMotorRight.setPower(-1);
+        }
+        else //stop when not in or out
+        {
+            intakeMotorLeft.setPower(0);
+            intakeMotorRight.setPower(0);
+        }
+    }
+}
+
